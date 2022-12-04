469caf6ac036e552703cb9c428aa3ca57a826929
Bruno <Bruno@Bruno-PC>
2022-12-04 11:24:18 -0300

    atualização

--- src/main/java/br/com/lojavirtual/LojaVirtualMentoriaApplication.java
+++ src/main/java/br/com/lojavirtual/LojaVirtualMentoriaApplication.java
@@ -2,8 +2,15 @@ package br.com.lojavirtual;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+
+/* 	@EntityScan(baseyPackages  = "br.com.lojavirtual.model")
+ *  utiliza esta anotação quando alguns computadores não fazem de forma automática
+ * 
+ * */
 @SpringBootApplication
+@EntityScan(basePackages  = "br.com.lojavirtual.model")
 public class LojaVirtualMentoriaApplication {
 
 	public static void main(String[] args) {
